5b2ba69b8e66202505f95a217d77e200651476eb
Rahul Sundaresan <rahul@rsun.dev>
2020-06-20 18:31:25 -0700

    forgot about two latencies. Doh!

--- mptcp_testing.py
+++ mptcp_testing.py
@@ -25,9 +25,9 @@ if __name__ == '__main__':
                 for error_rate in error_rate_list:
                     net = Mininet(link=TCLink, topo=TwoHostTwoLinkTopo(str(latency_1) + "ms", str(latency_2) + "ms", error_rate))
                     net.start()
-                    info(f"*** Iperf with {latency}ms and {error_rate}%\n")
+                    info(f"*** Iperf with {latency_1}ms {latency_2}ms and {error_rate}%\n")
                     server_bw_with_unit, _ = net.iperf(fmt='m', seconds=100)  # get the iperf server bandwidth results in Mbps. Ignore the client reported bandwidth since its inaccurate.
                     # server_bw_with_unit   is a string in the format of 'X Mbps'. We split the string just to get the X alone and convert the string to a float
                     server_bw = float(server_bw_with_unit.split(' ')[0])
-                    result_file.write(f"{latency},{error_rate},{server_bw}\n")
+                    result_file.write(f"{latency_1},{latency_2},{error_rate},{server_bw}\n")
                     net.stop()
