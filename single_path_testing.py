456e4d6c709ac2dde76a4beeea3244c01941e9c4
Rahul Sundaresan <rahul@rsun.dev>
2020-06-30 19:03:37 -0700

    got something to work

--- single_path_testing.py
+++ single_path_testing.py
@@ -6,6 +6,7 @@ from mininet.net import Mininet
 from statistics import mean, median
 from mininet.log import lg, info
 from math import ceil
+from time import sleep
 
 
 def min_seconds_to_transmit(bandwidth, tcp_mss=1480, packet_count=100000):
@@ -19,21 +20,28 @@ class TwoHostTopo(Topo):
         self.addLink(h1, h2, cls=TCLink, delay=latency, bw=bandwidth, loss=error_rate)
 
 
+    def test(self):
+        self.pingAll()
+
+
 if __name__ == '__main__':
     bandwidth_list = [10]  # bandwidth in Mbps, count 2
     latency_list = [1000, 1500, 2000, 2500]  # latency in ms. count 6
     error_rate_list = [0.1, 0.01, 0.001, 0.0001, 0.00001]  # error_rate in %, count 5
-    lg.setLogLevel('info')
+    lg.setLogLevel('debug')
 
     for bandwidth in bandwidth_list:
         for latency in latency_list:
             for error_rate in error_rate_list:
                 net = Mininet(controller=None, topo=TwoHostTopo(bandwidth, str(latency) + "ms", error_rate))
                 net.start()
-                h1, h2 = net.get("h1", "h2")
+                client, server = net.get("h1", "h2")
                 info(f"*** Iperf with {bandwidth}Mbps, {latency}ms and {error_rate}%\n")
-                h2.sendCmd("iperf3", {"-s": ""})
-                h1.sendCmd("iperf3", {"-c": h2.IP(), "--json": "", "--logfile": "~/MininetMultipathTCP/" + bandwidth
-                                                                                + latency + error_rate +".json"})
-                h2.sendInt
+                server.cmd('killall -9 iperf3')
+                server.sendCmd("iperf3 -s -P 1")
+                sleep(5)
+                print(f"Server IP is {server.IP()}")
+                h1_cmd = f"iperf3 --json -i -t 100 1 -c {server.IP()} --logfile test_{str(bandwidth)}_{str(latency)}_{str(error_rate)}.json"
+                client.cmdPrint(h1_cmd)
+                sleep(5)
                 net.stop()
